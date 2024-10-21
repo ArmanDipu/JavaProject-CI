b9285578d1364919ada5f5be0a412b33b9247fc6
ArmanDipu <armanhossain5@iut-dhaka.edu>
2024-10-21 22:05:05 +0600

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,10 @@ repositories {
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
+
 
 tasks.test {
     useJUnitPlatform()
